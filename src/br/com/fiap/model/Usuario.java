04bb84fbc7a8952dbea80a4a6bbf4b72d7d4c462
toterafa <79848666+toterafa@users.noreply.github.com>
2021-09-03 21:09:08 -0300

    atualizacao1

---
+++ src/br/com/fiap/model/Usuario.java
@@ -0,0 +1,50 @@
+package br.com.fiap.model;
+
+import java.time.LocalDate;
+
+public class Usuario {
+	private int id;
+	private String nome;
+	private LocalDate dataDeNascimento;
+	private String genero;
+	private String email;
+	private String senha;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public LocalDate getDataDeNascimento() {
+		return dataDeNascimento;
+	}
+	public void setDataDeNascimento(LocalDate dataDeNascimento) {
+		this.dataDeNascimento = dataDeNascimento;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+}
