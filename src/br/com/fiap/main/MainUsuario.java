4c65a4762bcb4704b8d3e5776ed7c7a597fa20d7
toterafa <79848666+toterafa@users.noreply.github.com>
2021-09-11 13:24:19 -0300

    inserção estadoUf e estadoCivil

--- src/br/com/fiap/main/MainUsuario.java
+++ src/br/com/fiap/main/MainUsuario.java
@@ -12,8 +12,10 @@ public class MainUsuario {
 		Usuario u1 = new Usuario();
 		u1.setNome("Andrézão da Massa");
 		u1.setId(123456);
-		u1.setDataDeNascimento(LocalDate.of(2000, 10, 01));
+		u1.setDataDeNascimento(LocalDate.of(2000, 10, 1));
 		u1.setGenero("Masculino");
+		u1.setEstadoUf("SP");
+		u1.setEstadoCivil("Casado");
 		u1.setEmail("andré@andré");
 		u1.setSenha("123");
 		
@@ -21,9 +23,11 @@ public class MainUsuario {
 		System.out.println(u1.getNome());
 		System.out.println(u1.getDataDeNascimento());
 		System.out.println(u1.getGenero());
+		System.out.println(u1.getEstadoUf());
+		System.out.println(u1.getEstadoCivil());
 		System.out.println(u1.getEmail());
 		System.out.println(u1.getSenha());
-	
+
 
 	}
 
