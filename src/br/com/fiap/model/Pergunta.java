184a3636662718dd495ab4b48479d16e01092f28
toterafa <79848666+toterafa@users.noreply.github.com>
2021-09-11 13:37:31 -0300

    inserção classe Pergunta

---
+++ src/br/com/fiap/model/Pergunta.java
@@ -0,0 +1,15 @@
+package br.com.fiap.model;
+
+public class Pergunta {
+    private String texto;
+
+
+    //getters
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+}
